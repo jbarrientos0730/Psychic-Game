3e34b51c829bbfc868ad32fa125224278af80c3f
Jorge Barrientos <jorgebarrientos0311@gmail.com>
2018-12-13 20:27:52 -0600

    vars

---
+++ assets/javascript/game.js
@@ -0,0 +1,7 @@
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+var computerGuess;
+var wins;
+var losses;
+var guesses;
+var lettersGuessed;
